1db05ced155abe19f60568857c6a57a3afe4dd26
David Selassie <selassid@gmail.com>
2013-04-15 11:21:22 -0700

    Adds tab-width.

--- config/selassid-conf.el
+++ config/selassid-conf.el
@@ -1,6 +1,7 @@
 (defvar autosave-dir "~/.live-packs/.auto-save-files/")
 (defvar backup-dir "~/.live-packs/.backup-files/")
 
+(setq-default tab-width 4)
 (setq-default vc-follow-symlinks t)
 (global-hl-line-mode t)
 (global-linum-mode t)
