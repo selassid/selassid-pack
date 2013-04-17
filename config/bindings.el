83968f3ec1d0de23a382f22e2fb917607ee6ce30
David Selassie <selassid@gmail.com>
2013-04-17 11:25:52 -0700

    Adds binding for comment-region.

--- config/bindings.el
+++ config/bindings.el
@@ -5,3 +5,5 @@
 ;;(define-key global-map (kbd "C--") 'text-scale-decrease)
 
 (define-key global-map (kbd "C-x C-d") (lambda () (interactive) (insert-debug-statement)))
+
+(define-key global-map (kbd "C-c C-c") 'comment-region)
