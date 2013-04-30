f0e22ccc3dc36402ea552148331f597636d10383
David Selassie <selassid@gmail.com>
2013-04-29 22:20:13 -0700

    Finally found an unused meta key combo for select word / symbol.

--- config/bindings.el
+++ config/bindings.el
@@ -2,8 +2,8 @@
 
 (define-key global-map (kbd "C-x C-d") 'insert-debug-statement)
 (define-key global-map (kbd "C-c C-c") 'comment-or-uncomment-region)
-(define-key global-map (kbd "M-W") 'er/mark-symbol)
-(define-key global-map (kbd "M-w") 'er/mark-word)
+(define-key global-map (kbd "M-Z") 'er/mark-symbol)
+(define-key global-map (kbd "M-z") 'er/mark-word)
 
 (define-key global-map (kbd "M-g") 'mc/edit-lines)
 (define-key global-map (kbd "C-M-s") 'mc/mark-next-like-this)
