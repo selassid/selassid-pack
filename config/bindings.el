6adaf733e0de4996aee4cf68f7888b7809ad369e
David Selassie <selassid@gmail.com>
2013-05-02 00:20:59 -0700

    Fixes edit-lines binding to not clash with goto-line.

--- config/bindings.el
+++ config/bindings.el
@@ -5,6 +5,6 @@
 (define-key global-map (kbd "M-Z") 'er/mark-symbol)
 (define-key global-map (kbd "M-z") 'er/mark-word)
 
-(define-key global-map (kbd "M-g") 'mc/edit-lines)
+(define-key global-map (kbd "C-x C-l") 'mc/edit-lines)
 (define-key global-map (kbd "C-M-s") 'mc/mark-next-like-this)
 (define-key global-map (kbd "C-M-r") 'mc/mark-previous-like-this)
