7d5eeaf2e61e1dd78ec940cb24aeb700068688f5
David Selassie <selassid@gmail.com>
2013-08-27 16:36:42 -0700

    Removes comment binding. We already have M-/.

--- config/bindings.el
+++ config/bindings.el
@@ -1,7 +1,6 @@
 ;; Place your bindings here.
 
 (define-key global-map (kbd "C-x C-d") 'insert-debug-statement)
-(define-key global-map (kbd "C-c C-c") 'comment-or-uncomment-region)
 (define-key global-map (kbd "M-Z") 'er/mark-symbol)
 (define-key global-map (kbd "M-z") 'er/mark-word)
 
