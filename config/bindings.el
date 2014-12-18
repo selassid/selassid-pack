c00d77df12bf2c678fc79d078435f1772a3bddb4
David Selassie <selassid@gmail.com>
2014-12-18 15:03:31 -0800

    Adds window switch and this buffer kill binding.

--- config/bindings.el
+++ config/bindings.el
@@ -1,9 +1,11 @@
-;; Place your bindings here.
-
 (define-key global-map (kbd "C-x C-d") 'insert-debug-statement)
+
 (define-key global-map (kbd "M-Z") 'er/mark-symbol)
 (define-key global-map (kbd "M-z") 'er/mark-word)
 
 (define-key global-map (kbd "C-x C-l") 'mc/edit-lines)
 (define-key global-map (kbd "C-M-s") 'mc/mark-next-like-this)
 (define-key global-map (kbd "C-M-r") 'mc/mark-previous-like-this)
+
+(define-key global-map (kbd "C-x k") 'kill-this-buffer)
+(define-key global-map (kbd "M-o") 'win-switch-next-window)
