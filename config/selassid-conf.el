f69f6e93e3955dce45b8e453aac46c8a60479224
David Selassie <selassid@gmail.com>
2014-12-18 15:14:40 -0800

    Adds my eshell customizations.

--- config/selassid-conf.el
+++ config/selassid-conf.el
@@ -23,6 +23,43 @@
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
 (setq mouse-wheel-progressive-speed nil)
 
+(setq eshell-directory-name "~/.eshell")
+(setq eshell-cmpl-ignore-case t)
+
+(setq eshell-banner-message "")
+(require 'em-smart)
+(setq eshell-where-to-jump 'begin)
+(setq eshell-review-quick-commands nil)
+(setq eshell-smart-space-goes-to-end t)
+
+(defmacro ds-fg-color (str fg-color)
+  `(propertize ,str 'face (list :foreground ,fg-color)))
+
+(setq colorhash-1 "#ffa033")
+(setq colorhash-2 "#33d6ff")
+(setq colorhash-3 "#334eff")
+
+(defvar ds-pwd-color colorhash-1)
+(defvar ds-git-color colorhash-2)
+
+(defun ds-pwd-prompt ()
+  (ds-fg-color (abbreviate-file-name (eshell/pwd)) ds-pwd-color))
+(defun ds-git-prompt ()
+  (if (magit-get-top-dir)
+      (concat "("
+              (ds-fg-color (magit-get-current-branch) ds-git-color)
+              (if (magit-anything-staged-p) (ds-fg-color "S" "green") "")
+              (if (magit-anything-unstaged-p) (ds-fg-color "U" "red") "")
+              ")")
+    ""))
+(defun ds-sent-prompt ()
+  (if (= (user-uid) 0)
+      (ds-fg-color " # " "red")
+    " $ "))
+(setq eshell-prompt-function (lambda nil
+                               (concat (ds-pwd-prompt)
+                                       (ds-git-prompt)
+                                       (ds-sent-prompt))))
 
 (defun insert-debug-statement ()
   (interactive)
